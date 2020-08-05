e9cdb4c88701428281d9c7c4e9ae0473ed8f0ce7
Julie Uchitel <juchitel1@users.noreply.github.com>
2020-08-05 10:03:36 +0100

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 77b0696d4def12295b8c4654690b511c60677510, reversing
    changes made to d083d10778396a293a5f51f6b04bd3fa0dc773f2.

--- LUMO_toolbox/DOTHUB_LUMOSD2layout.m
+++ LUMO_toolbox/DOTHUB_LUMOSD2layout.m
@@ -11,8 +11,7 @@ function DOTHUB_LUMOSD2layout(SDinput2D,SDinput3D,groupid,outname)
 %               probe layout information (usually phantom-measured for a 
 %               given cap). If 3D positions are not available, just parse
 %               the 2D SD in stead.
-%groupid    =   The intended group ID for this layout (in decimal).
-%               Defaults to 1
+%groupid    =   The intended group ID for this layout (in decimal)
 
 %outname    =   The intended output name for the new .json file. Default is
 %               'LUMO_LayoutFile_' datestr(clock) '.JSON'
