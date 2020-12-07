2f7552a34f310a8952a98b69b93d8e004b75e988
RJCooperUCL <robert.cooper@ucl.ac.uk>
2020-12-07 18:17:38 +0000

    Example Pipeline developments

--- PipelineExample1.m
+++ PipelineExample1.m
@@ -1,26 +1,49 @@
-% DOTHUB Recon Pipeline Example 1.
+% DOT-HUB toolbox Pipeline Example 1.
 %
-% Individual optode positioning information, subject-specific .mshs file.
-% LUMO DATA
+% What follows is an example of a wrapper script that employs the main 
+% steps of the toolbox. Most steps output variables into the work
+% space and writes them as the key file types, so you can easily comment 
+% out steps as you work through them and pick up where you left off, rather
+% than re-running every step. The whole script runs in ~12 minutes on a 208
+% MacBook Pro with 16Gb RAM.
 
+% Example 1 is the simplest application of the toolbox to LUMO data. It is
+% assumed that we have no subject-specific information about the position
+% of the optodes (and therefore 3D positioning information is derived from
+% the default 3D values in  the LUMOcap .json file) nor do we have
+% subject-specific structural MRI; we use an adult atlas.
+%
+% The dataset is an adult visual eccentricity experiment equivalent to that
+% described in Vidal-Rosas et. al. 2021(?) Neurophotonics (in review):
+% "Evaluating a new generation of wearable high-density diffuse optical
+% tomography technology via retinotopic mapping of the adult visual cortex"
+%
+% RJC, UCL, Dec 2020.
 
+%% Specify paths of pre-defined elements (.LUMO, atlas .mshs, Homer2 preprocessing .cfg file).
+[filepath,~,~] = fileparts(mfilename('fullpath'));
+LUMODirName = [filepath '/ExampleData/Example1/Example1_VisualEccentricity.LUMO'];
+origMeshFileName = [filepath '/ExampleMeshes/AdultMNI152.mshs'];
+cfgFileName = [filepath '/ExampleData/Example1/preproPipelineExample1.cfg'];
 
+%% Covert .LUMO to .nirs
+[nirs, nirsFileName, SD3DFileName] = DOTHUB_LUMO2nirs(LUMODirName);
 
+%% Run data quality checks
+%DOTHUB_dataQualityCheck(nirsFileName);
 
+%disp('Examine data quality figures, press any key to continue');
+%pause 
 
-
-
-%% function [prepro, preproFileName] = examplePreProcessingScript(nirsFileName)
+%% Run Homer2 pre-processing pipeline line by line, then write .prepro file:
+% dod = hmrIntensity2OD(nirs.d);
+% SD3D = enPruneChannels(nirs.d,nirs.SD3D,ones(size(nirs.t)),[0 1e6],12,[0 100],1); 
+% dod = hmrBandpassFilt(dod,nirs.t,0,0.5);
+% dc = hmrOD2Conc(dod,SD3D,[6 6]);
+% dc = DOTHUB_hmrSSRegressionByChannel(dc,SD3D,11,4); %This is a custom SS regression script. 
+% [dcAvg,dcAvgStd,tHRF] = hmrBlockAvg(dc,nirs.s,nirs.t,[-5 25]);
 % 
-% load(nirsFileName,'-mat');
-% 
-% od = hmrIntensity2OD(d);
-% od = hmrBandpassFilt(od,1/mean(diff(t)),0.01,0.5);
-% dc = hmrOD2Conc(od,SD3D,[6 6]);
-% [dcAvg, dcStd, tHRF] = hmrBlockAvg(dc,s,t,[-5 25]);
-% dodAvg = DOTHUB_hmrHRFConc2OD(dcAvg, SD3D,[6 6]);
-% 
-% %USE CODE SNIPPET FROM DOTHUB_writePREPRO to define filename and logData
+% % Use code snippet from DOTHUB_writePREPRO to define contents of logs:
 % [pathstr, name, ~] = fileparts(nirsFileName);
 % ds = datestr(now,'yyyymmDDHHMMSS');
 % preproFileName = fullfile(pathstr,[name '.prepro']);
@@ -28,7 +51,36 @@
 % logData(2,:) = {'Derived from data: ', nirsFileName};
 % logData(3,:) = {'Pre-processed using:', mfilename('fullpath')};
 % 
-% %DOTHUB_writePREPRO(preproFileName,logData,SD3D,tDOD,dod,tHRF,dcAvg,dcStd)
 % [prepro, preproFileName] = DOTHUB_writePREPRO(preproFileName,logData,SD3D,tHRF,dodAvg,tHRF,dcAvg,dcStd);
-% 
-% end
+
+% Alternatively, you can run a Homer2 pipeline based on a .cfg file and
+% create a .prepro file automatically using:
+[prepro, preproFileName] = DOTHUB_runHomerPrepro(nirsFileName,cfgFileName);
+
+%% Register chosen mesh to subject SD3D and create rmap
+[rmap, rmapFileName] = DOTHUB_meshRegistration(nirsFileName,origMeshFileName);
+DOTHUB_plotRMAP(rmap)
+
+%% Calculate Jacobian 
+basis = [30 30 30];
+[jac, jacFileName] = DOTHUB_makeToastJacobian(rmapFileName,basis);
+
+%% Invert Jacobian
+%Note that you can either separately calculate the inverse, or just run
+%DOTHUB_reconstruction, which will then call the inversion itself
+[invjac, invjacFileName] = DOTHUB_invertJacobian(jacFileName,preproFileName,'saveFlag',true,'reconMethod','multispectral','hyperParameter',0.01);
+
+%% Reconstruction
+%Reconstruct
+[dotimg, dotimgFileName] = DOTHUB_reconstruction(preproFileName,[],invjacFileName,rmapFileName,'saveVolumeImages',true);
+
+%% Display peak response results on atlas surface and in volume
+timeRange = [10 15]; %seconds post-onset
+fs = length(dotimg.tImg)./range(dotimg.tImg);
+frameRange = round((frameTime + abs(min(dotimg.tImg))).*fs);
+frames = frameRange(1):frameRange(2);
+DOTHUB_plotSurfaceDOTIMG(dotimg,origMeshFileName,frames,'condition',3,'view',[0 20]);
+DOTHUB_plotVolumeDOTIMG(dotimg,origMeshFileName,frames);
+
+
+
---
+++ PipelineTmp.m
@@ -0,0 +1,34 @@
+% DOTHUB Recon Pipeline Example 1.
+%
+% Individual optode positioning information, subject-specific .mshs file.
+% LUMO DATA
+
+
+
+
+
+
+
+
+%% function [prepro, preproFileName] = examplePreProcessingScript(nirsFileName)
+% 
+% load(nirsFileName,'-mat');
+% 
+% od = hmrIntensity2OD(d);
+% od = hmrBandpassFilt(od,1/mean(diff(t)),0.01,0.5);
+% dc = hmrOD2Conc(od,SD3D,[6 6]);
+% [dcAvg, dcStd, tHRF] = hmrBlockAvg(dc,s,t,[-5 25]);
+% dodAvg = DOTHUB_hmrHRFConc2OD(dcAvg, SD3D,[6 6]);
+% 
+% %USE CODE SNIPPET FROM DOTHUB_writePREPRO to define filename and logData
+% [pathstr, name, ~] = fileparts(nirsFileName);
+% ds = datestr(now,'yyyymmDDHHMMSS');
+% preproFileName = fullfile(pathstr,[name '.prepro']);
+% logData(1,:) = {'Created on: '; ds};
+% logData(2,:) = {'Derived from data: ', nirsFileName};
+% logData(3,:) = {'Pre-processed using:', mfilename('fullpath')};
+% 
+% %DOTHUB_writePREPRO(preproFileName,logData,SD3D,tDOD,dod,tHRF,dcAvg,dcStd)
+% [prepro, preproFileName] = DOTHUB_writePREPRO(preproFileName,logData,SD3D,tHRF,dodAvg,tHRF,dcAvg,dcStd);
+% 
+% end
