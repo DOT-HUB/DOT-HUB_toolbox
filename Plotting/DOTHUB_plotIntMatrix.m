f62ac9859fed34f369d446d919c69ef4b563c357
RJCooperUCL <robert.cooper@ucl.ac.uk>
2022-11-24 22:48:46 +0000

    Update to include how I would code this

--- Plotting/DOTHUB_plotIntMatrix.m
+++ Plotting/DOTHUB_plotIntMatrix.m
@@ -21,6 +21,34 @@ function DOTHUB_plotIntMatrix(d,SD)
 % Updated by GCVL 2022 Cambridge to handle n wavelengths
 % #########################################################################
 
+
+
+
+%How Rob would do it ###################
+% dMatOrig = log10(mean(d,1));
+% 
+% nWav = max(SD.MeasList(:,4));
+% nRowsPerWav = size(d,2)/nWav;
+% for i = 1:nWav
+%     dMat = reshape(dMatOrig((i-1)*nRowsPerWav+1:i*nRowsPerWav),SD.nDets,SD.nSrcs)';
+% 
+%     ax(i) = subplot(1,nWav,i);
+%     set(gcf,'color','w');
+%     imagesc(ax(i),dMat);axis square
+%     caxis(ax(i),[-6 0]);
+%     cb1 = colorbar(ax(i),'northoutside');
+%     ylabel(cb1,['-log10(intensity)' num2str(SD.Lambda(i)) 'nm'],'FontSize',12)
+%     colormap(ax(i),'gray')
+%     set(ax(i),'Box','on','FontSize',12,'XTick',1:SD.nDets,'YTick',1:SD.nSrcs);
+%     axis(ax(i), 'tight');
+%     xlabel(ax(i),'Detector','FontSize',12);
+%     ylabel(ax(i),'Source','FontSize',12);
+% 
+% end
+%###################
+
+
+
 dMat = log10(mean(d,1));
 
 if length(SD.Lambda) == 2
